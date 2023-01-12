e87d8ca6ac61ea6c0ee27a8d42413279e62d1ba3
William Cannon <william.cannon@gmail.com>
2023-01-12 09:02:33 -0600

    major progress

--- asg_info.py
+++ asg_info.py
@@ -81,6 +81,54 @@ def get_vendor_tag_value(image_location, ami_name, owner_id):
   '''fill out with logic later on - returns true, false, tbd'''
   return "true"
 
+def create_instance_tags_list(response, VMA_value):
+  '''Inspect a response, and return the correct TagSpecifications when adding our Vendor_Managed_AMI tag'''
+  TagSpecifications = []
+  lt_template_data = response['LaunchTemplateVersions'][0]['LaunchTemplateData']
+  if 'TagSpecifications' not in lt_template_data.keys(): # no tags on any resource types at all
+    TagSpecifications.append({'ResourceType': 'instance',
+                               'Tags' : [ {'Key': 'Vendor_Managed_AMI', 'Value': VMA_value}]})
+  else: # we preserve existing tags, and add ours to it
+    tags_list = lt_template_data.get('TagSpecifications', None) # tags_list is a list of dicts
+    for tags_dict in tags_list:
+      if tags_dict.get('ResourceType') != "instance": # keep other types intact
+        TagSpecifications.append(tags_dict)
+      else: # we need to add our key/value and preserve the other instance tags
+        new_tags_dict = {'ResourceType' : 'instance'}
+        new_tags = tags_dict['Tags'][:] # get a copy, preserves all the current tags
+        new_tags.append( {'Key':'Vendor_Managed_AMI', 'Value': VMA_value})
+        new_tags_dict['Tags'] = new_tags
+    TagSpecifications.append(new_tags_dict)
+  return TagSpecifications 
+
+def create_new_launch_template(ec2_client, lt_name, lt_version, lt_dict):
+  '''Create a new launch template, simply updating it with our new tag dictionary'''
+  response = ec2_client.create_launch_template_version(LaunchTemplateName=lt_name,
+                                            SourceVersion=lt_version,
+                                            LaunchTemplateData=lt_dict)
+  return response
+
+def update_asg_launch_template_version(asg_client, asg_name, template_name, template_version):
+    '''Update the asg to use a new template version'''
+    # e.g. asg set to use specific version, we modify it to use our new version number
+    response = asg_client.update_auto_scaling_group(
+        AutoScalingGroupName=asg_name,
+        LaunchTemplate={
+            'LaunchTemplateName': template_name,
+            'Version': template_version
+     })
+    return response
+
+def update_launch_template_default(ec2_client, template_name, new_lt_version):
+    version_update_response = ec2_client.modify_launch_template(
+        DryRun=False,
+        #ClientToken='string',
+        #LaunchTemplateId='string',
+        LaunchTemplateName=template_name,
+        DefaultVersion=str(new_lt_version)
+        )
+    return version_update_response
+
 def main(dry_run=True, num_asg=None, asg_name=None, region=None):
     ec2_client = boto3.client('ec2', region_name=region)
     asg_client = boto3.client('autoscaling', region_name=region)
@@ -144,9 +192,14 @@ def update_asg_tag(ec2_client=None, asg_client=None, region=None, asg=None):
     template_name = asg['LaunchTemplate']['LaunchTemplateName']
     template_version = asg['LaunchTemplate']['Version']
     print("-"*80)
+    print(f"asg template_version to use: {template_version}")
+    print("-"*80)
     lt_info = get_lt_info(ec2_client, template_name, template_version)
     image_id = lt_info['LaunchTemplateVersions'][0]['LaunchTemplateData']['ImageId']
-
+    lt_version = str(lt_info['LaunchTemplateVersions'][0]['VersionNumber'])
+    print("-"*80)
+    print(f"lt_version: {lt_version}")
+    print("-"*80)
     print("Launch Template Info")
     pprint.pprint(lt_info)
     print("-"*80)
@@ -155,14 +208,35 @@ def update_asg_tag(ec2_client=None, asg_client=None, region=None, asg=None):
     vma_exists = determine_if_VMA_tag_exists(lt_info)
     print(f"VMA tag exists: {vma_exists}")
     if vma_exists: # don't need to add a tag
+        print(f"Vendor_Managed_AMI tag exists for launch template: {template_name}")
         return
 
     print("-"*80)
     image_location, ami_name, owner_id = get_ami_info(ec2_client, image_id)
-    print(f"image_location: {image_location}\n, ami_name: {ami_name}\n, owner_id: {owner_id}\n")
+    print(f"image_location: {image_location}\n ami_name: {ami_name}\n owner_id: {owner_id}\n")
     print("-" * 80)
-    vma_tag_value = get_vendor_tag_value(image_location, ami_name, owner_id)
+    vma_tag_value = get_vendor_tag_value(image_location, ami_name, owner_id) # will be true or false or tbd
     print(f"vma_tag_value: {vma_tag_value}")
+    print("-" * 80)
+    TagSpecifications = create_instance_tags_list(lt_info, vma_tag_value)   
+    lt_dict = {'TagSpecifications': TagSpecifications}
+    new_lt = create_new_launch_template(ec2_client, template_name, lt_version, lt_dict)
+    new_lt_version = new_lt['LaunchTemplateVersion']['VersionNumber']
+    print("*" * 80)
+    print(f"New launch template version: {new_lt_version}")
+    print("-"*80)
+    if template_version.isdigit(): # have to update the asg to use new version number
+        print("Updating ASG to use our new version number")
+        print(asg_client, asg_name, template_name, new_lt_version)
+        r1 = update_asg_launch_template_version(asg_client, asg_name, template_name, str(new_lt_version))
+        #pprint.pprint(r1)
+    elif template_version == "$Default": # have to update launch template value of $Default
+        print("Updating $Default to have our latest version number")
+        update_launch_template_default(ec2_client, template_name, str(new_lt_version))
+    elif template_version == "$Latest": # no work to do, will use our newer version
+        print("ASG set to use latest, continuing")
+    else:
+        print("Should not see this printed - new corner case found")
     return
 
 
@@ -207,7 +281,7 @@ In a region, gather all asgs
 
 ** Should also support:
 - dry run - just show what would be done
-- limit to number of ASGs to act on (e.g. only run against X ASGs, then exit)
+- DONE limit to number of ASGs to act on (e.g. only run against X ASGs, then exit)
 
 
 
