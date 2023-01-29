5b3db85b4b87b39a985ff5cd84474afcf1846d5f
William Cannon <william.cannon@gmail.com>
2023-01-29 11:28:34 -0600

    enough

--- dynamodb_get_set.py
+++ dynamodb_get_set.py
@@ -3,10 +3,29 @@ import boto3
 
 
 TABLE_NAME = "audit_log"
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-table = dynamodb.Table('audit_log')
-table.put_item(
-    Item = {'timestamp': '2023-01-29'}
-)
+
+def put_latest(table, value):
+    table.put_item(
+        Item = {'timestamp': 'latest', 'date_time': value}
+    )
+
+def get_latest(table):
+    resp = table.get_item(
+                Key = {'timestamp': 'latest'}
+            )
+    return resp
+
+if __name__ == "__main__":
+    # get a connection to dynamodb
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+    # push our data into the table
+    table = dynamodb.Table('audit_log')
+    put_latest(table, '12345679')
+
+    response = get_latest(table)
+    print(response['Item'])
+
+
 
 
